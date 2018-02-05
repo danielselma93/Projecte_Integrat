bbd484692ead98171df2bf400744e1542d6c6e72
Dani <danielselma93@gmail.com>
2018-02-05 15:55:23 +0100

    Proba persona

---
+++ app/src/main/java/com/example/paco/projecte_integrat/Persona.java
@@ -0,0 +1,12 @@
+package com.example.paco.projecte_integrat;
+
+/**
+ * Created by Dani on 05/02/2018.
+ */
+
+public class Persona {
+    String nom;
+    String apellido;
+    String correo;
+    
+}
